d9d3447654d7dc43c00f078d1b095769a17007d4
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2018-12-09 14:39:12 -0500

    use bytestring builder - first version that output some hex value

--- src/Main.hs
+++ src/Main.hs
@@ -3,34 +3,37 @@
 -- license MIT https://raw.githubusercontent.com/fromjavatohaskell/hexdump/master/LICENSE-MIT
 
 import           Data.ByteString ( ByteString )
+import           Data.ByteString.Builder ( Builder )
 import           Data.Maybe ( listToMaybe )
 import           Control.Monad ( void )
 import qualified System.Environment as E
 import qualified Data.ByteString as BS hiding (scanl')
+import qualified Data.ByteString.Builder as BS
+import           System.IO ( BufferMode(..) )
+import qualified System.IO as IO
 
-data Chunk = Chunk {-# UNPACK #-} !Int !ByteString !ByteString
+data Chunk = Chunk !Bool {-# UNPACK #-} !Int !ByteString !ByteString
 
 chunkSize :: Int
 chunkSize = 16
 
 initialChunk :: ByteString -> Chunk
 initialChunk x = f $ BS.splitAt chunkSize x
-  where f (a, b) = Chunk 0 a b
+  where f (a, b) = Chunk True 0 a b
 
 nextChunk :: Chunk -> Chunk
-nextChunk (Chunk offset head rest) = f $ BS.splitAt chunkSize rest
-  where f (a, b) = Chunk (offset + BS.length head) a b 
+nextChunk (Chunk ne offset head rest) = f $ BS.splitAt chunkSize rest
+  where f (a, b) = Chunk (ne && prevNonEmpty) (offset + BS.length head) a b 
+        prevNonEmpty = not $ BS.null head
 
 notEmpty :: Chunk -> Bool
-notEmpty (Chunk _ head _) = not $ BS.null head
+notEmpty (Chunk ne _ head _) = ne
 
 split :: ByteString -> [Chunk]
 split x = takeWhile notEmpty $ iterate nextChunk $ initialChunk x
 
-instance Show Chunk where
-  show = showChunk
-
-showChunk (Chunk offset chunk _) = show offset
+toBuilder :: Chunk -> Builder
+toBuilder (Chunk _ offset chunk _) = BS.intDec offset <> BS.char8 ' ' <> BS.byteStringHex chunk <> BS.char8 '\n'
 
 main :: IO ()
 main = do
@@ -38,4 +41,5 @@ main = do
   d <- case listToMaybe args of
     Just filename -> BS.readFile filename
     Nothing -> BS.getContents
-  void $ traverse (putStrLn . show) (split d)
+  IO.hSetBuffering IO.stdout $ BlockBuffering $ Just $ 1024 * 16
+  void $ traverse (BS.hPutBuilder IO.stdout . toBuilder) (split d)
