07f0bd7b66d60a2ca6bf4147881eecc5fb054738
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2018-12-09 13:23:00 -0500

    implement stream of chunks

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,35 @@
 
 -- license MIT https://raw.githubusercontent.com/fromjavatohaskell/hexdump/master/LICENSE-MIT
 
+import           Data.ByteString ( ByteString )
 import           Data.Maybe ( listToMaybe )
+import           Control.Monad ( void )
 import qualified System.Environment as E
-import qualified Data.ByteString as BS
+import qualified Data.ByteString as BS hiding (scanl')
+
+data Chunk = Chunk {-# UNPACK #-} !Int !ByteString !ByteString
+
+chunkSize :: Int
+chunkSize = 16
+
+initialChunk :: ByteString -> Chunk
+initialChunk x = f $ BS.splitAt chunkSize x
+  where f (a, b) = Chunk 0 a b
+
+nextChunk :: Chunk -> Chunk
+nextChunk (Chunk offset head rest) = f $ BS.splitAt chunkSize rest
+  where f (a, b) = Chunk (offset + BS.length head) a b 
+
+notEmpty :: Chunk -> Bool
+notEmpty (Chunk _ head _) = not $ BS.null head
+
+split :: ByteString -> [Chunk]
+split x = takeWhile notEmpty $ iterate nextChunk $ initialChunk x
+
+instance Show Chunk where
+  show = showChunk
+
+showChunk (Chunk offset chunk _) = show offset
 
 main :: IO ()
 main = do
@@ -12,4 +38,4 @@ main = do
   d <- case listToMaybe args of
     Just filename -> BS.readFile filename
     Nothing -> BS.getContents
-  BS.putStr d
+  void $ traverse (putStrLn . show) (split d)
