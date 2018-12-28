bc241024b59dc2dbb3a77971fda0232c6c33c9a6
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2018-12-27 19:58:49 -0500

    complete rewrite of buildOffset function, remove unsafe IO

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, MagicHash #-}
+{-# LANGUAGE OverloadedStrings, MagicHash, LambdaCase #-}
 
 -- license MIT https://raw.githubusercontent.com/fromjavatohaskell/hexdump/master/LICENSE-MIT
 
@@ -10,21 +10,13 @@ import           Data.ByteString.Builder        ( Builder )
 import           Data.Word                      ( Word8 )
 import           Data.Maybe                     ( listToMaybe )
 import           Data.Foldable                  ( traverse_ )
-import           Control.Monad                  ( void )
-import           Foreign                        ( ForeignPtr(..) )
-import           Foreign.Storable               ( peekElemOff )
-import           Foreign.ForeignPtr.Unsafe      ( unsafeForeignPtrToPtr )
-import           System.IO.Unsafe               ( unsafePerformIO )
 import qualified System.Environment            as E
-import qualified Data.ByteString               as BS
-import qualified Data.ByteString.Internal      as BS
 import qualified Data.ByteString.Lazy          as BSL
 import qualified Data.ByteString.Builder       as B
 import           System.IO                      ( BufferMode(..) )
 import qualified System.IO                     as IO
 
-data Chunk = Chunk {-# UNPACK #-} !Int64 !ByteString
---data Chunk = Chunk Int64 ByteString
+data Chunk = Chunk Int64 ByteString
 
 chunkSize :: Int64
 chunkSize = 16
@@ -39,30 +31,23 @@ filterPrintable :: Word8 -> Word8
 filterPrintable x | x >= 0x20 && x <= 0x7e = x
                   | otherwise              = 0x2e
 
-uncheckedIShiftRL :: Int64 -> Int# -> Int64
-uncheckedIShiftRL (I64# n) i = I64# (uncheckedIShiftRL# n i)
+hexEncodeLowerNibble :: Int64 -> Word8
+hexEncodeLowerNibble x = 
+  let nibble = fromIntegral $ x .&. 0xF in
+  if nibble < 0xa then (charNumberOffset + nibble) else (charLetterOffset + nibble)
+  where
+    charNumberOffset = fromIntegral $ fromEnum '0'
+    charLetterOffset = fromIntegral $ fromEnum 'a' - 0xa
 
 buildOffset :: Int64 -> Builder
-buildOffset offset = buildOffset' 0 offset
-  where
-    buildOffset' counter offset
-      | counter < 6 || offset > 0 = buildOffset' (counter + 1) (uncheckedIShiftRL offset 4#)
-        <> B.word8 (hexEncodeLowerNibble offset)
+buildOffset offset = buildOffset' (offset >>> 24#)
+  <> (B.int8HexFixed $ fromIntegral $ (offset >>> 16#))
+  <> (B.int16HexFixed $ fromIntegral offset)
+  where 
+    (I64# n) >>> i = I64# (uncheckedIShiftRL# n i)
+    buildOffset' offset
+      | offset /= 0 = buildOffset' (offset >>> 4#) <> B.word8 (hexEncodeLowerNibble offset)
       | otherwise = mempty
-
-newtype EncodingTable = EncodingTable (ForeignPtr Word8)
-
-tableFromList :: [Word8] -> EncodingTable
-tableFromList xs = case BS.pack xs of BS.PS fp _ _ -> EncodingTable fp
-
-unsafeIndex :: EncodingTable -> Int -> IO Word8
-unsafeIndex (EncodingTable table) = peekElemOff (unsafeForeignPtrToPtr table)
-
-lowerAlphabet :: EncodingTable
-lowerAlphabet = tableFromList $ map (fromIntegral . fromEnum) $ ['0'..'9'] ++ ['a'..'f']
-
-hexEncodeLowerNibble :: Int64 -> Word8
-hexEncodeLowerNibble x = unsafePerformIO $ unsafeIndex lowerAlphabet $ fromIntegral $ x .&. 0xF
 
 hex :: ByteString -> Builder
 hex chunk = BSL.foldr singleSymbol mempty chunk
@@ -99,9 +84,8 @@ toBuilder (Chunk offset chunk) =
 
 main :: IO ()
 main = do
-  args <- E.getArgs
-  d    <- case listToMaybe args of
+  IO.hSetBuffering IO.stdout $ BlockBuffering $ Just $ 1024 * 32
+  dataIn <- fmap listToMaybe E.getArgs >>= \case
     Just filename -> BSL.readFile filename
     Nothing       -> BSL.getContents
-  IO.hSetBuffering IO.stdout $ BlockBuffering $ Just $ 1024 * 32
-  traverse_ (B.hPutBuilder IO.stdout . toBuilder) (chunked 0 d)
+  traverse_ (B.hPutBuilder IO.stdout . toBuilder) (chunked 0 dataIn)
