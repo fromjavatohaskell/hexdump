2f0dc91dea1ee2d0f14270601e04e52f9e38ca37
FromJavaToHaskell <fromjavatohaskell@gmail.com>
2019-03-17 19:55:38 -0400

    refactoring of encodeOffset function

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,14 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE MagicHash #-}
 
-
 -- license MIT https://raw.githubusercontent.com/fromjavatohaskell/hexdump/master/LICENSE-MIT
 
-import           GHC.Int                        ( Int64(..) )
 import           GHC.Word                       ( Word64(..), Word8(..) )
 import           Data.Bits                      ( Bits(..), (.&.) )
 import           GHC.Prim                       ( Int#, uncheckedShiftRL#, clz64# )
-import           Data.ByteString.Lazy           ( ByteString )
 import           Data.Maybe                     ( listToMaybe )
+import           Control.Monad                  ( when )
 import qualified System.Environment            as E
-import qualified Data.ByteString.Lazy          as BSL
 import           System.IO                      ( BufferMode(..) )
 import qualified System.IO                     as IO
 import qualified GHC.IO.Buffer                 as Buf
@@ -35,9 +32,6 @@ tableHex !index = Buf.peekByteOff (unsafeForeignPtrToPtr table) index
 
 chunkSize :: Int
 chunkSize = 16
-
-chunkSize64 :: Int64
-chunkSize64 = fromIntegral chunkSize
 
 (>>>) :: Word64 -> Int# -> Word64
 (W64# n) >>> i = W64# (uncheckedShiftRL# n i)
@@ -64,83 +58,76 @@ hexDigit !x = do
 encodeOffset :: Word64 -> Ptr Word8 -> Int -> IO Int
 encodeOffset offset buffer len = do
   let decodeNibbles = max 6 (16 - (fromIntegral ((clz offset) >>> 2#))) :: Int
-  encodeOffset' offset buffer (len + decodeNibbles - 1) decodeNibbles
+  encodeOffset' offset (len + decodeNibbles - 1) decodeNibbles
   return $! len + decodeNibbles
+ where 
+  encodeOffset' !offset' !index !decodeNibbles
+   | decodeNibbles > 0 = do
+     !hexTemp <- hexDigit $! fromIntegral offset'
+     Buf.pokeByteOff buffer index hexTemp
+     encodeOffset' (offset' >>> 4#) (index - 1) (decodeNibbles - 1)
+   | otherwise = do
+     return ()
 {-# INLINE encodeOffset #-}
 
-encodeOffset' :: Word64 -> Ptr Word8 -> Int -> Int -> IO ()
-encodeOffset' !offset !buffer !index !decodeNibbles
-  | decodeNibbles > 0 = do
-    !hexTemp <- hexDigit $! fromIntegral offset
-    Buf.pokeByteOff buffer index hexTemp
-    encodeOffset' (offset >>> 4#) buffer (index - 1) (decodeNibbles - 1)
-  | otherwise = do
-    return ()
-{-# INLINE encodeOffset' #-}
 
-
-encode :: Word64 -> ByteString -> Int -> Ptr Word8 -> IO (Int)
-encode offset inData chunkLength buffer
-  | BSL.null inData = do
-     len <- encodeOffset offset buffer 0
-     Buf.pokeByteOff buffer len (0x0a :: Word8)
+encode :: Word64 -> Ptr Word8 -> Int -> Ptr Word8 -> IO (Int)
+encode !offset bufferIn !chunkLength bufferOut
+  | (chunkLength == 0) = do
+     len <- encodeOffset offset bufferOut 0
+     Buf.pokeByteOff bufferOut len (0x0a :: Word8)
      return (len + 1)
   | otherwise = do
-     len <- encodeOffset offset buffer 0
-     Buf.pokeByteOff buffer len (0x20 :: Word8)
+     len <- encodeOffset offset bufferOut 0
+     Buf.pokeByteOff bufferOut len (0x20 :: Word8)
      let len2 = len + 1
      len3 <- encodeByte 0 len2
 
      len4 <- if chunkLength < chunkSize then pad 0 (3 * (chunkSize - chunkLength)) len3
         else return len3
      
-     Buf.pokeByteOff buffer len4 (0x20 :: Word8)
+     Buf.pokeByteOff bufferOut len4 (0x20 :: Word8)
      let len5 = len4 + 1
-     Buf.pokeByteOff buffer len5 (0x3e :: Word8)
+     Buf.pokeByteOff bufferOut len5 (0x3e :: Word8)
      let len6 = len5 + 1
 
      len7 <- encodeAscii 0 len6
-     Buf.pokeByteOff buffer len7 (0x3c :: Word8)
+     Buf.pokeByteOff bufferOut len7 (0x3c :: Word8)
      let len8 = len7 + 1
-     Buf.pokeByteOff buffer len8 (0x0a :: Word8)
+     Buf.pokeByteOff bufferOut len8 (0x0a :: Word8)
      let len9 = len8 + 1
      return len9
   where
  encodeByte !index !len
   | index < chunkLength = do
-     let oneByte = BSL.index inData (fromIntegral index)
+     oneByte <- (Buf.peekByteOff bufferIn index) :: IO Word8
      hexTemp1 <- hexDigit (oneByte >>>| 4#)
      hexTemp2 <- hexDigit oneByte
-     Buf.pokeByteOff buffer len hexTemp1
-     Buf.pokeByteOff buffer (len+1) hexTemp2
-     Buf.pokeByteOff buffer (len+2) (0x20 :: Word8)
+     Buf.pokeByteOff bufferOut len hexTemp1
+     Buf.pokeByteOff bufferOut (len+1) hexTemp2
+     Buf.pokeByteOff bufferOut (len+2) (0x20 :: Word8)
      encodeByte (index + 1) (len + 3)
   | otherwise = return len
  encodeAscii !index !len
   | index < chunkLength = do
-    let oneByte = BSL.index inData (fromIntegral index)
+    oneByte <- (Buf.peekByteOff bufferIn index) :: IO Word8
     filteredAscii <- tableConvertAscii $ fromIntegral oneByte
-    Buf.pokeByteOff buffer len filteredAscii
+    Buf.pokeByteOff bufferOut len filteredAscii
     encodeAscii (index + 1) (len + 1)
   | otherwise = return len
  pad !bytesToPad totalBytesToPad !len
   | bytesToPad < totalBytesToPad = do
-    Buf.pokeByteOff buffer len (0x20 :: Word8)
+    Buf.pokeByteOff bufferOut len (0x20 :: Word8)
     pad (bytesToPad + 1) totalBytesToPad (len + 1)
   | otherwise = return len
 {-# NOINLINE encode #-}
 
-encodeStream :: Word64 -> ByteString -> Ptr Word8 -> IO ()
-encodeStream offset inData buffer
-  | BSL.null inData = do
-     len <- encode offset inData 0 buffer
-     IO.hPutBuf IO.stdout buffer len
-  | otherwise = do
-     let (as, zs) = BSL.splitAt chunkSize64 inData
-     let chunkLength = (fromIntegral $ BSL.length as) :: Int
-     len <- encode offset as chunkLength buffer
-     IO.hPutBuf IO.stdout buffer len
-     encodeStream (offset + fromIntegral chunkLength) zs buffer
+encodeStream :: Word64 -> IO.Handle -> Ptr Word8 -> Ptr Word8 -> IO ()
+encodeStream offset h bufferIn bufferOut = do
+  chunkLength <- IO.hGetBuf h bufferIn chunkSize
+  len <- encode offset bufferIn chunkLength bufferOut
+  IO.hPutBuf IO.stdout bufferOut len
+  when (chunkLength > 0) $ encodeStream (offset + fromIntegral chunkLength) h bufferIn bufferOut
 
 filterPrintable :: Int -> Word8
 filterPrintable x = if (x >= 0x20 && x <= 0x7e) then (fromIntegral x) else 0x2e
@@ -159,9 +146,11 @@ tableConvertAscii !index = Buf.peekByteOff (unsafeForeignPtrToPtr tableAscii) in
 main :: IO ()
 main = do
   h <- getFilename >>= getHandle
-  IO.hSetBuffering IO.stdout $ BlockBuffering $ Just $ 1024 * 64
-  IO.hSetBuffering h $ BlockBuffering $ Just $ 1024 * 64
-  inData <- BSL.hGetContents h
-  buf <- Buf.newByteBuffer 1024 Buf.WriteBuffer
-  Buf.withBuffer buf $ encodeStream 0 inData
-
+  setBuffering IO.stdout
+  setBuffering h
+  bufIn <- Buf.newByteBuffer chunkSize Buf.WriteBuffer
+  bufOut <- Buf.newByteBuffer 1024 Buf.WriteBuffer
+  Buf.withBuffer bufIn  $ \ bufferIn -> Buf.withBuffer bufOut $ \ bufferOut -> do
+    encodeStream 0 h bufferIn bufferOut
+ where
+    setBuffering handle = IO.hSetBuffering handle $ BlockBuffering $ Just $ 1024 * 64
